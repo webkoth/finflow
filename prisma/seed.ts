caca9ec0c97ec2713266b537548a552a15f584dd
Webkoth <webkoth@gmail.com>
2026-07-13 17:07:15 +0300

    fix: корректный порядок disconnect/exit в seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -37,8 +37,8 @@ async function main() {
 }
 
 main()
+  .finally(() => prisma.$disconnect())
   .catch((e) => {
     console.error(e);
     process.exit(1);
-  })
-  .finally(() => prisma.$disconnect());
+  });
